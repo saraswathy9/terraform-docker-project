24016b522a24eebe10c97ba5c860762ba98de490
saraswathy9 <144031748+saraswathy9@users.noreply.github.com>
2026-08-20 12:02:40 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,53 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
 
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_security_group" "web_sg" {
+  name        = "docker-web-sg"
+  description = "Allow HTTP and SSH"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "docker_web" {
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
+  user_data              = file("${path.module}/user_data.sh")
+
+  tags = {
+    Name = "terraform-docker-webserver"
+  }
+}
+
+output "public_ip" {
+  value = aws_instance.docker_web.public_ip
+}
