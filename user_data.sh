014e5ad1aed97457a4d2e1c006a3a2c36ee5f36c
saraswathy9 <144031748+saraswathy9@users.noreply.github.com>
2026-08-20 12:03:10 +0530

    Update user_data.sh

--- user_data.sh
+++ user_data.sh
@@ -1,1 +1,26 @@
+#!/bin/bash
+yum update -y
+yum install -y docker
+systemctl start docker
+systemctl enable docker
 
+mkdir -p /home/ec2-user/app
+cd /home/ec2-user/app
+
+cat > index.html << 'HTMLEOF'
+<!DOCTYPE html>
+<html>
+<head><title>Terraform + Docker + httpd</title></head>
+<body style="font-family: sans-serif; text-align:center; margin-top: 100px;">
+  <h1>Deployed with Terraform + Docker (Apache httpd)</h1>
+</body>
+</html>
+HTMLEOF
+
+cat > Dockerfile << 'DOCKEREOF'
+FROM httpd:alpine
+COPY index.html /usr/local/apache2/htdocs/index.html
+DOCKEREOF
+
+docker build -t my-web-app .
+docker run -d -p 80:80 --restart always my-web-app
